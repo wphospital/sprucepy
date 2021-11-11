59234bc2d7c1909360f2e9bcf116eea8fc89e2d4
Sege <jsege@wphospital.org>
2021-11-11 15:52:50 -0500

    **debugging** print stderr and stdout

--- sprucepy/runner.py
+++ sprucepy/runner.py
@@ -169,7 +169,6 @@ class Runner:
         # TODO: change API to query params like Recipients??
         ept = urljoin(api_url, run_ept) + '/' + self.run_id.__str__()
 
-        print('1')
         print(res)
 
         status = RETURN_CODES.get(res.returncode, 'fail')
@@ -177,11 +176,11 @@ class Runner:
         if status == 'fail':
             self.notify_failure(res)
 
-        print('2')
-        print(res)
-
         error = res.stderr.read().decode('ascii')
         output = res.stdout.read().decode('ascii')
+
+        print(error)
+        print(output)
 
         payload = dict(
             end_time=datetime.now(timezone.utc),
@@ -240,7 +239,7 @@ class Runner:
         run_url = urljoin(app_url, 'tasks/runs/') + self.run_id.__str__()
         task_url = urljoin(app_url, 'tasks/') + self.task_id.__str__()
         # error_str=res.stderr.decode('ascii').replace('\n', '<br>')
-        error_str = res.stderr.read().decode('ascii')
+        error_str = res.stderr.read().decode('ascii').replace('\n', '<br>')
         # error_str = res.stderr
 
         print(error_str)
