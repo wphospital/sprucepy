01ff89396890836b5987bd71e01e5612081bfbeb
Sege <jsege@wphospital.org>
2021-11-10 22:55:28 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sprucepy",
-    version="0.0.5",
+    version="0.0.6",
     author="Jon Sege",
     author_email="jsege@wphospital.org",
     description="Spruce package",
