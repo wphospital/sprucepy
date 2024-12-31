249e19cad787a6b0212615087a5125d9147a0c1f
Jon Sege <jsege@wphospital.org>
2024-12-31 10:04:41 -0500

    change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sprucepy",
-    version="0.1",
+    version="0.2",
     author="Jon Sege",
     author_email="jsege@wphospital.org",
     description="Spruce package",
