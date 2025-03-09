ed97d69d0074622d5189b5f429fcc450536c2133
Jon Sege <jsege@wphospital.org>
2025-03-09 11:01:55 -0400

    add auth header

--- sprucepy/secrets.py
+++ sprucepy/secrets.py
@@ -3,14 +3,41 @@ import os
 from .constants import api_url
 
 
-def get_secret_by_key(key, api_url=api_url):
+def get_secret_by_key(
+    key,
+    api_url : str = api_url,
+    api_token : str = ''
+):
     """Retrieve a secret from the key vault
 
     Args:
-        key (str): the key for the secret to retreive
+        key
+            str: the key for the secret to retreive
+        api_url
+            str : the root url for the Spruce API
+        api_token
+            str : auth token for the Spruce API
+
+    Returns:
+        str
+            the secret value
     """
-    res = requests.get(api_url + "secrets/" + str(key))
+
+    auth_token = os.getenv('SPRUCE_API_TOKEN', api_token)
+
+    headers = {
+        'Authorization': f'access_token {auth_token}'
+    }
+
+    res = requests.get(
+        api_url + "secrets/" + str(key),
+        headers=headers
+    )
+
     if res.status_code == 200:
         return res.json()['value']
     else:
-        raise IndexError(key)
+        if res.status_code == 404:
+            raise IndexError(key)
+        else:
+            raise Exception(res.text)
